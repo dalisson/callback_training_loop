60f8e554245082fcf61b542ea1fa2575ba228bf7
dalisson figueiredo <dalisson@mindsdigital.net>
2020-04-13 14:49:38 -0300

    optimizer updated

--- training_library/modelsettings/setoptimizer.py
+++ training_library/modelsettings/setoptimizer.py
@@ -5,9 +5,9 @@ class SetOptimizerCallback(Callback):
     '''
     sets the optimizer
     '''
-    def begin_fit(self, **kwargs):
+    def begin_fit(self):
         all_parameters = [self.run.model]
         if hasattr(self.run.loss_func, 'params'):
             all_parameters.append(self.run.loss_func)
         self.run.optim = discriminative_lr_optimizer(all_parameters, self.run.lr,
-                                                     self.run.optim, **kwargs)
+                                                     self.run.optim)
