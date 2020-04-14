0205afe19db59485e2fc1f0d96582f20e930e9fe
dalisson figueiredo <dalisson@mindsdigital.net>
2020-04-14 14:03:34 -0300

    modelsetting callback fixed

--- training_library/modelsettings/settrainablemodules.py
+++ training_library/modelsettings/settrainablemodules.py
@@ -4,4 +4,4 @@ class SetTrainableModulesCallback(Callback):
 
     def begin_fit(self):
         if hasattr(self.run.loss_func, 'parameters'):
-            self.run.trainable_modules = self.run.trainable_modules.append(self.run.loss_func)
+            self.run.trainable_modules.append(self.run.loss_func)
