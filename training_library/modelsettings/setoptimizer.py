ac06992979534c43eec2f6495da96ef977d88aee
dalisson figueiredo <dalisson@mindsdigital.net>
2020-04-13 22:15:54 -0300

    setoptimizercallback changed

--- training_library/modelsettings/setoptimizer.py
+++ training_library/modelsettings/setoptimizer.py
@@ -5,9 +5,18 @@ class SetOptimizerCallback(Callback):
     '''
     sets the optimizer
     '''
+    def __init__(self, **kwargs):
+        super(SetOptimizerCallback, self).__init__()
+        self.kwargs = kwargs
+
     def begin_fit(self):
-        all_parameters = [self.run.model]
-        if hasattr(self.run.loss_func, 'params'):
-            all_parameters.append(self.run.loss_func)
-        self.run.optim = discriminative_lr_optimizer(all_parameters, self.run.lr,
-                                                     self.run.optim)
+        '''
+        sets up the optimizer to the model parameters,
+        additionaly if the loss function has trainable parameters
+        these a sent to optimizizer as well
+        '''
+        trainable_modules = [self.run.model]
+        if hasattr(self.run.loss_func, 'parameters'):
+            trainable_modules.append(self.run.loss_func)
+        self.run.optim = discriminative_lr_optimizer(trainable_modules, self.run.lr,
+                                                     self.run.optim, **self.kwargs)
