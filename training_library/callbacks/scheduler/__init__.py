02f0b25b968535b9fdcad81f3fe4f529abffee89
dalisson figueiredo <dalisson@mindsdigital.net>
2020-04-15 16:17:24 -0300

    scheduler folder structure changed

--- training_library/callbacks/scheduler/__init__.py
+++ training_library/callbacks/scheduler/__init__.py
@@ -1,1 +1,2 @@
-from .scheduler import *
+from .scheduler import ParamScheduler
+from .annealingfunctions import sched_lin, sched_cos, sched_no, sched_exp
--- training_library/callbacks/scheduler/scheduler.py
+++ training_library/callbacks/scheduler/scheduler.py
@@ -1,13 +1,13 @@
-from ..imports import torch
 from ..callback import Callback
-from functools import partial
-import math
 
-__all__ = ['ParamScheduler', 'sched_lin', 'sched_cos', 'sched_no', 'sched_exp']
+
+__all__ = ['ParamScheduler']
 
 class ParamScheduler(Callback):
-    _order=1
-    def __init__(self, pname, sched_func): self.pname,self.sched_func = pname,sched_func
+    order=1
+    def __init__(self, pname, sched_func): 
+        super(ParamScheduler, self).__init__()
+        self.pname,self.sched_func = pname,sched_func
 
     def set_param(self):
         for pg in self.opt.param_groups:
@@ -16,28 +16,3 @@ class ParamScheduler(Callback):
     def begin_batch(self): 
         if self.in_train: self.set_param()
 
-
-def annealer(f):
-    def _inner(start, end): return partial(f, start, end)
-    return _inner
-
-@annealer
-def sched_lin(start, end, pos): return start + pos*(end-start)
-@annealer
-def sched_cos(start, end, pos): return start + (1 + math.cos(math.pi*(1-pos))) * (end-start) / 2
-@annealer
-def sched_no(start, end, pos):  return start
-@annealer
-def sched_exp(start, end, pos): return start * (end/start) ** pos
-
-
-def combine_scheds(pcts, scheds):
-    assert sum(pcts) == 1.
-    pcts = torch.tensor([0] + pcts)
-    assert torch.all(pcts >= 0)
-    pcts = torch.cumsum(pcts, 0)
-    def _inner(pos):
-        idx = (pos >= pcts).nonzero().max()
-        actual_pos = (pos-pcts[idx]) / (pcts[idx+1]-pcts[idx])
-        return scheds[idx](actual_pos)
-    return _inner
