83594e9049c57df5cc073511abfc17613805a71a
dalisson figueiredo <dalisson@mindsdigital.net>
2020-04-16 09:34:51 -0300

    scheduler updated

--- training_library/callbacks/scheduler/scheduler.py
+++ training_library/callbacks/scheduler/scheduler.py
@@ -9,9 +9,14 @@ class ParamScheduler(Callback):
         super(ParamScheduler, self).__init__()
         self.pname,self.sched_func = pname,sched_func
 
+    def begin_fit(self):
+        if not isinstance(self.sched_func, list):
+            self.sched_func = [self.sched_func] * self.run.n_param_groups
+
     def set_param(self):
-        for pg in self.opt.param_groups:
-            pg[self.pname] = self.sched_func(self.iter/self.total_iter)
+        assert self.run.n_param_groups == len(self.sched_funcs)
+        for pg, func in zip(self.optim.param_groups, self.sched_func):
+            pg[self.pname] = func(self.iter/self.total_iter)
 
     def begin_batch(self): 
         if self.in_train: self.set_param()
