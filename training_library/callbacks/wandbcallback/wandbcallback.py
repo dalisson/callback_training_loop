5b7b13b207869e9910c65a8c0061c00bc9e9c613
dalisson figueiredo <dalisson@mindsdigital.net>
2020-04-20 15:08:05 -0300

    wandb callback changed

--- training_library/callbacks/wandbcallback/wandbcallback.py
+++ training_library/callbacks/wandbcallback/wandbcallback.py
@@ -3,6 +3,9 @@ from ..callback import Callback
 
 
 class WandbCallback(Callback):
+    '''
+    Callback to allow monitoring the model through Wandb
+    '''
     order = 10
     def __init__(self, configs, wandb_project, wandb_name, entity="minds"):
         super(WandbCallback, self).__init__()
@@ -11,7 +14,7 @@ class WandbCallback(Callback):
         self.wandb_name = wandb_name
         self.entity = entity
         self.config = None
-        self.metrics = None
+        self.stage = 'train'
 
     def begin_fit(self):
         wandb.init(name=self.wandb_name, project=self.wandb_project, entity=self.entity)
@@ -21,5 +24,23 @@ class WandbCallback(Callback):
         self.config = config
         wandb.watch(self.run.model, log='all')
 
+    def begin_epoch(self):
+        '''
+        set staget to train
+        '''
+        self.stage = 'train'
+
+    def begin_eval(self):
+        '''
+        set stage to eval
+        '''
+        self.stage = 'eval'
+
     def after_all_batches(self):
-        wandb.log(self.run.metrics)
+        '''
+        Logs to wandbd after all batches are completed
+        '''
+        log = dict()
+        for key in self.run.metrics[self.stage].keys():
+            log[('%s_%s' % (self.stage, key))] = self.run.metrics[self.stage][key][-1]
+        wandb.log(log)
