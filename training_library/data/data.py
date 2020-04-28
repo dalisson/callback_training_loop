77e5373b06d79b462f8847c46e82cc46c2caa6a0
dalisson figueiredo <dalisson@mindsdigital.net>
2020-04-28 12:09:28 -0300

    fix show image batch

--- training_library/data/data.py
+++ training_library/data/data.py
@@ -8,9 +8,10 @@ class Data(object):
 
     '''
 
-    def __init__(self, dataloaders, n_classes=None):
+    def __init__(self, dataloaders, n_classes=None, data_type='img'):
         self.train_dl, self.valid_dl = dataloaders
         self.n_classes = n_classes
+        self.data_type = data_type
 
     @classmethod
     def from_audio_images(cls, train_dir, test_dir, b_size, data_aug=True):
@@ -26,16 +27,29 @@ class Data(object):
             n_classes = len(list(classes.keys()))
         else:
             n_classes = None
-        return cls(dataloaders=dataloaders, n_classes=n_classes)
+        return cls(dataloaders=dataloaders, n_classes=n_classes, data_type='img')
 
-    def show_batch(self, figsize=None):
-        figsize = (15,60) if not figsize else figsize
+    def show_batch(self, **kwargs):
+        '''
+        Show a batch of samples
+        '''
+        if self.data_type == 'img':
+            self._show_image_batch(**kwargs)
+        else:
+            return 'Not Implemented for Data Type'
+
+    def _show_image_batch(self, figsize=None):
+        '''
+        Show a batch of images
+        '''
+        figsize = (8, 8) if not figsize else figsize
         x, y = next(iter(self.train_dl))
-        x, y = x[:4], y[:4]
-
-        _, axes = plt.subplots(2,2, figsize=figsize)
-        for ax, samples in zip(axes.flatten(), (x,y)):
-            s_x, s_y = samples
-            transposed = np.transpose(s_x.detach().cpu().numpy(), (1,2,0)) 
+        _, axes = plt.subplots(8, 2, figsize=figsize)
+        for i, ax in enumerate(axes.flatten()):
+            s_x, s_y = x[i], y[i]
+            transposed = np.transpose(s_x.detach().cpu().numpy(), (1, 2, 0))
+            n_channels = transposed.shape[-1]
+            if n_channels == 1:
+                transposed = np.squeeze(transposed, axis=-1)
             ax.imshow(transposed)
             ax.set_title(s_y.detach().cpu().numpy())
