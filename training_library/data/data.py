d4e7de13dcb1860fb89bc3f6c250bffad882abe4
dalisson figueiredo <dalisson@mindsdigital.net>
2020-04-28 10:45:08 -0300

    Data classmethod updated

--- training_library/data/data.py
+++ training_library/data/data.py
@@ -10,12 +10,12 @@ class Data(object):
         self.train_dl, self.valid_dl = dataloaders
         self.n_classes = n_classes
 
-    def get_n_classes(self):
-        assert self.n_classes is not None
-        return self.n_classes
-
     @classmethod
-    def build_softmax_data_audioimage(cls, train_dir, test_dir, b_size, data_aug=True):
+    def from_audio_images(cls, train_dir, test_dir, b_size, data_aug=True):
+        '''
+        Builds Data class for audio images to be used with Softmax
+
+        '''
         dataloaders = build_softmax_image_dataloader(train_dir=train_dir,
                                                      test_dir=test_dir,
                                                      batch_size=b_size,
