a523d831f4a8ff50a2f7d3b4de420c09587a0344
dalisson figueiredo <dalisson@mindsdigital.net>
2020-04-30 11:12:13 -0300

    train on resnetse34l added

--- training_library/callbacks/modelsettings/resnetse34lt.py
+++ training_library/callbacks/modelsettings/resnetse34lt.py
@@ -3,4 +3,4 @@ from ..callback import Callback
 class Resnetse34(Callback):
 
     def begin_batch(self):
-        self.run.x_batch = self.run.x_batch.permute((0,2,3,1))
+        self.run.x_batch = self.run.x_batch.permute((0,2,3,1)).squeeze(1)
