f0e3592c318d3bfa8720d860fa9f991acf60ae62
dalisson figueiredo <dalisson@mindsdigital.net>
2020-05-06 17:23:52 -0300

    xresnet arch added to library

--- training_library/models/__init__.py
+++ training_library/models/__init__.py
@@ -1,2 +1,6 @@
+'''
+Module contains instances of ready to use archtectures
+'''
 from .vladnetwork import VladNetwork
 from .resnet34se import resnetse34, resnetse34l, VGGVox
+from .xresnet import xresnet18, xresnet34, xresnet50, xresnet101, xresnet152
---
+++ training_library/models/xresnet/__init__.py
@@ -0,0 +1,4 @@
+'''
+The Xresnet archtecture as seen on bag of tricks paper
+'''
+from .xresnet import xresnet101, xresnet152, xresnet18, xresnet34, xresnet50
